c003dbe06edb52b5b3185741c965bb9ec2465ca4
sanghvirajit19 <sanghvirajit19@gmail.com>
2021-12-16 00:16:18 +0100

    inital commit

--- BlackScholes.py
+++ BlackScholes.py
@@ -17,8 +17,10 @@ def blackscholes(r, S, K, T, sigma, type='C'):
     d1 = (np.log(S / K) + (r + sigma ** 2 / 2) * T) / (sigma * np.sqrt(T))
     d2 = d1 - sigma * np.sqrt(T)
     try:
+        # Call option
         if type == 'C':
             price = S * norm.cdf(d1, 0, 1) - K * np.exp(-r * T) * norm.cdf(d2, 0, 1)
+        # Put option
         elif type == 'P':
             price = K * np.exp(-r * T) * norm.cdf(-d2, 0, 1) - S * norm.cdf(d1, 0, 1)
         return price
